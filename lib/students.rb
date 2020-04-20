8b3ce4f39f5aec4e7c06636029d11442679643de
Ray Alva <rayalva407@gmail.com>
2020-04-20 15:37:10 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,15 +1,19 @@
 def highest_student_gpa
-  "SELECT MAX(gpa) FROM students"
+  "SELECT MAX(gpa) FROM student;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end
